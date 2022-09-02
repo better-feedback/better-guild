9168a05e716553fa81834f03c3a4885738fa74c1
seichris <cerujurasa@gmail.com>
2022-09-02 12:53:06 +0200

    custom config

--- src/config.ts
+++ src/config.ts
@@ -22,14 +22,14 @@ export const nearChainConfig = {
 };
 
 export const siteConfig = {
-  enabledChains: ["near" , "polygon"],
-  projectName: "Better",
-  title: "Better Roadmap",
-  metaDescription: "Better Roadmap",
+  enabledChains: ["near"],
+  projectName: "Guild.xyz",
+  title: "Guild Roadmap",
+  metaDescription: "Guild Roadmap",
   url: "https://roadmap.betterhq.org/",
   metaImg: "https://raw.githubusercontent.com/better-feedback/better-app/1926cf0a2327e629128f65e57edeee7440294e0b/public/metaTagImg.jpg?token=AF6ZD6A7XI4IS7I3MRLDQSDCTY6QK",
   externalLinks: {
-    docs: "https://github.com/better-feedback/roadmap#readme",
+    docs: "https://github.com/better-feedback/better-guild#readme",
     discord: "wwwwRFa6aj",
     twitter: "betterdao",
   },
